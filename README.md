14a884ad1e842b4a89a9c083efcf0c8ca9be3391
Timna Schulze <Timna.lansel@gmail.com>
2023-01-29 00:17:30 -0800

    add html questions, css style, and js formulas

---
+++ README.md
@@ -0,0 +1,34 @@
+# _Which Programming Language to Learn_
+
+#### By _**Timna Schulze**_
+
+#### _This is the second Independent Project and Code Review for Intro to Programming at Epicodus_
+
+## Link to site on GitHub Pages
+
+https://github.com/TimnaSchulze/Code-Language
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _JavaScript_
+
+## Description
+
+_This webpage uses JavaScript Forms to input answers to questions about the experience and interests of the user to decide which programming language to study._
+
+## Setup/Installation Requirements
+
+* _Clone repository to your desktop._
+* _Open ts/index.html in your browser._
+
+## Known Bugs
+
+* _I am not getting a result. This form isn't working yet._
+
+## License
+
+_For contributions or corrections, please contact Timna at timna.lansel@gmail.com_
+
+Copyright (c) _Jan 29, 2023_ _Timna Schulze_
---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+form.onload = function() {
+  const form = document.querySelector("form");
+  form.onsubmit = function(event) {
+event.preventDefault();
+  };
+};
+
+function language() {
+  let history = document.querySelector("input[name='history']:checked").value;
+  let schedule = document.querySelector("input[name='schedule']:checked").value;
+  let math = document.querySelector("input[name='math']:checked").value;
+  let skills = document.querySelector("input[name='skills']:checked").value;
+  let personality = document.querySelector("input[name='personality']:checked").value;
+
+  let language;
+  if (history === "customerService" && schedule === "nineToFive" && math === "algebra" && skills === "newbie" && personality === "introvert") {
+      language = "Python";
+  } else if (history === "deskJob" && schedule === "asNeeded" && math === "geometry" && skills === "familiar" && personality === "extravert") {
+      language = "Ruby";
+  } else if (history === "management" && schedule === "collaboration" && math === "calculus" && skills === "programmer" && personality === "both") {
+      language = "Java";
+  } else {
+      language = "C";
+  }
+
+  document.getElementById("submit").innerHTML = "You should start with " + language + "!";
+}
+
+document.getElementById("tryAgain").addEventListener("click", function(){
+  let form = document.querySelector("form");
+  form.reset();
+
+  document.getElementById("result").innerHTML = "";
+});
